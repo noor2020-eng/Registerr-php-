fd0552a0263b56211cc29245398a7872b7589973
Noor M. I. Albardawil <67733645+noor2020-eng@users.noreply.github.com>
2020-07-16 12:13:19 +0300

    first commit

---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<head>
+  <title>LOG IN</title>
+  <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+    .brand{
+      background: #18ffff  !important;
+    }
+    .brand-text{
+      color:#18ffff  !important;
+    }
+    .input-text{
+    	color: #18ffff  !important;
+    }
+    form{
+      max-width: 460px;
+      margin: 20px auto;
+      padding: 20px;
+    }
+   
+    </style>
+</head>
+<body class="white lighten-4">
+  <nav class="black z-depth-0">
+    <div class="container">
+      <a href="index.php" class="brand-logo brand-text center">LOG IN</a>
+    </div>
+  </nav>
